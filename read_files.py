86724c15dbd2b2bebe94a216ab09860d6ac92ec4
qianshangzen <qianshangzen@gmail.com>
2021-02-17 12:12:18 -0800

    updated data with reading function

--- read_files.py
+++ read_files.py
@@ -1,14 +1,19 @@
 
-
 import os
 import pandas as pd
+import itertools as it
+
+def customized_pd_read(file_path, hdr):
+    print("Reading file " + file_path)
+    return pd.read_csv(file_path, header = hdr)
 
 files = os.listdir()
 file_names = [i for i in files if bool(re.search("book[0-9]*.txt", i))]
-data = pd.concat(map(pd.read_csv, file_names))
+data = pd.concat(it.starmap(customized_pd_read, list(map(lambda x: (x, None), file_names))))
+print(f'Finished reading {len(data)} books')
+
+data.columns = ['Subject','Title','Author','Url','Cover_url','Price','ISBN-10','ISBN-13','PubDate','Publisher','Overview']
 data = data.drop_duplicates(['Title'])
 data = data.reset_index().drop(['index'], axis = 1)
-
-print(f'Total {len(data)} numbers of books')
-print('Writing to txt file...')
 data.to_csv('final_data.txt')
+print(f'After cleaning, total {len(data)} of books is saved to "final_data.txt"')
